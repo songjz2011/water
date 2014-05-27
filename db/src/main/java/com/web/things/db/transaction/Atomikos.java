279fd2db46f95d380541cbfcbf73d00c10e10b04
songjz <songjz2011@163.com>
2014-05-27 17:41:48 +0800

    modify

---
+++ db/src/main/java/com/web/things/db/transaction/Atomikos.java
@@ -0,0 +1,17 @@
+package com.web.things.db.transaction;
+
+
+/**
+ * <pre>
+ * 类描述
+ * </pre>
+ * 
+ * @author songjz
+ * @time 2014年5月27日
+ */
+public class Atomikos {
+	public static void main(String[] args) throws Exception {
+		
+	}
+		
+}
